a73b5d28d24ff0423a80f2b7860770d9ab7fccac
kyle <kyleghafari@gmail.com>
2020-01-29 15:50:06 -0500

    First commit. Just setting up repo. Code to come soon

---
+++ Lab 9 - Refactoring the Student List project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab_9___Refactoring_the_Student_List_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
